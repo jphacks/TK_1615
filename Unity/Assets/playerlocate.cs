4f35a62b1d0ce09a1d4c8d915585013c6246d629
YuIshikawa <s1320793@u.tsukuba.ac.jp>
2016-11-04 23:09:54 +0900

    copy and paste ueda's code into this branch code

--- Unity/Assets/playerlocate.cs
+++ Unity/Assets/playerlocate.cs
@@ -1,54 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerlocate : MonoBehaviour {
+public class playerlocate : MonoBehaviour
+{
 
-	GameObject[] playerObjects;
-	int playerNum;
+    GameObject[] playerObjects;
+    int playerNum;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update(){
+    // Update is called once per frame
+    void Update()
+    {
 
-		//playerNumはログイン後しばらく0なのでUpdate側で数値の変化をチェック
+        //playerNumはログイン後しばらく0なのでUpdate側で数値の変化をチェック
 
-		if (playerNum == 0) {
-		
-			playerObjects = GameObject.FindGameObjectsWithTag ("Player");
-			playerNum = playerObjects.Length;
-			Debug.Log (playerNum);
+        if (playerNum == 0)
+        {
 
-			if (playerNum == 1) {
-				this.transform.position = new Vector3 (0, 0, 4);
-				float y = 180;
-				this.transform.rotation = Quaternion.Euler (0.0f, y, 0.0f);
-			} else if (playerNum == 2) {
-				this.transform.position = new Vector3 (0, 0, -4);
-				this.transform.rotation = Quaternion.identity;
-			} else if (playerNum == 3) {
-				this.transform.position = new Vector3 (0, 4, 0);
-				this.transform.rotation = Quaternion.identity;
-				float y = -90;
-				this.transform.rotation = Quaternion.Euler (0.0f, y, 0.0f);
-			} else if (playerNum == 4) {
-				this.transform.position = new Vector3 (0, -4, 0);
-				this.transform.rotation = Quaternion.identity;
-				float y = 90;
-				this.transform.rotation = Quaternion.Euler (0.0f, y, 0.0f);
-			} else if (playerNum > 4) {
-				//5人目以降。時間があれば同心円上に並べたい…
-				this.transform.position = new Vector3 (0, Random.Range (-4f, 4f), Random.Range (-4f, 4f));
-				this.transform.rotation = Quaternion.identity;
-				float y = Random.Range (0, 360);
-				this.transform.rotation = Quaternion.Euler (0.0f, y, 0.0f);
-			}
-		
-		}
+            playerObjects = GameObject.FindGameObjectsWithTag("Player");
+            playerNum = playerObjects.Length;
 
-	}
+            if (playerNum == 1)
+            {
+                this.transform.position = new Vector3(0, 2, 4);
+                this.transform.LookAt(new Vector3(0, 2, 0));
+                Debug.Log("photon login :" + playerNum);
+                Debug.Log("【Unity】: Start Record");
+                SwiftClass.swiftStartRecordingMethod();
+            }
+            else if (playerNum == 2)
+            {
+                this.transform.position = new Vector3(0, 2, -4);
+                this.transform.LookAt(new Vector3(0, 2, 0));
+                Debug.Log("photon login :" + playerNum);
+                Debug.Log("【Unity】: Start Record");
+                SwiftClass.swiftStartRecordingMethod();
+            }
+            else if (playerNum == 3)
+            {
+                this.transform.position = new Vector3(4, 2, 0);
+                this.transform.LookAt(new Vector3(0, 2, 0));
+                Debug.Log("photon login :" + playerNum);
+                Debug.Log("【Unity】: Start Record");
+                SwiftClass.swiftStartRecordingMethod();
+            }
+            else if (playerNum == 4)
+            {
+                this.transform.position = new Vector3(-4, 2, 0);
+                this.transform.LookAt(new Vector3(0, 2, 0));
+                Debug.Log("photon login :" + playerNum);
+                Debug.Log("【Unity】: Start Record");
+                SwiftClass.swiftStartRecordingMethod();
+            }
+            else if (playerNum > 4)
+            {
+                //5人目以降。時間があれば同心円上に並べたい…
+                this.transform.position = new Vector3(Random.Range(-4f, 4f), 2, Random.Range(-4f, 4f));
+                this.transform.LookAt(new Vector3(0, 2, 0));
+            }
+
+            //MonsterFireでCameracontrallerが飛んでっちゃうの防ぐ
+            Vector3 pos = transform.position;
+            pos.y = 2;
+            transform.position = pos;
+        }
+
+    }
 
 }
