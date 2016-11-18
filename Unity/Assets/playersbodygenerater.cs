f08a87eb49af81097ae6909129ead65921873de2
YuIshikawa <s1320793@u.tsukuba.ac.jp>
2016-11-18 18:22:56 +0900

    fix the bug of color of users
    to show the identical color by users

--- Unity/Assets/playersbodygenerater.cs
+++ Unity/Assets/playersbodygenerater.cs
@@ -3,61 +3,80 @@ using System.Collections;
 
 public class playersbodygenerater : MonoBehaviour {
 
-	public GameObject player1;
-	public GameObject player2;
-	public GameObject player3;
-	public GameObject player4;
-	public GameObject player5;
-	public bool color = false;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+    public GameObject player5;
+    public bool color = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (color == false) {
-			Vector3 playerpos = this.transform.position;
-			Quaternion playerrot = this.transform.rotation;
-			int Pnum = playerlocate.Pnum;
+        if (color == false)
+        {
 
-			if (Pnum != 0) {
-				if (Pnum == 1) {
-					// プレハブからインスタンスを生成
-					GameObject obj = (GameObject)Instantiate (player1, playerpos, playerrot);
-					// 作成したオブジェクトを子として登録
-					obj.transform.parent = transform;
-					color = true;
-				} else if (Pnum == 2) {
-					// プレハブからインスタンスを生成
-					GameObject obj = (GameObject)Instantiate (player2, playerpos, playerrot);
-					// 作成したオブジェクトを子として登録
-					obj.transform.parent = transform;
-					color = true;
-				} else if (Pnum == 3) {
-					// プレハブからインスタンスを生成
-					GameObject obj = (GameObject)Instantiate (player3, playerpos, playerrot);
-					// 作成したオブジェクトを子として登録
-					obj.transform.parent = transform;
-					color = true;
-				} else if (Pnum == 4) {
-					// プレハブからインスタンスを生成
-					GameObject obj = (GameObject)Instantiate (player4, playerpos, playerrot);
-					// 作成したオブジェクトを子として登録
-					obj.transform.parent = transform;
-					color = true;
-				} else if (Pnum == 5) {
-					// プレハブからインスタンスを生成
-					GameObject obj = (GameObject)Instantiate (player5, playerpos, playerrot);
-					// 作成したオブジェクトを子として登録
-					obj.transform.parent = transform;
-					color = true;
-				}
-			}
-		}
+            Vector3 playerpos = this.transform.position;
+            Quaternion playerrot = this.transform.rotation;
+            int Pnum = playerlocate.Pnum;
 
-	}
+            if (Pnum != 0)
+            {
+                if (Pnum == 1)
+                {
+                    // プレハブからインスタンスを生成
+                    GameObject obj = (GameObject)Instantiate(player1, playerpos, playerrot);
+                    // 作成したオブジェクトを子として登録
+                    obj.transform.parent = transform;
+                    color = true;
+                    Pnum = 0;
+                }
+                else if (Pnum == 2)
+                {
+                    // プレハブからインスタンスを生成
+                    GameObject obj = (GameObject)Instantiate(player2, playerpos, playerrot);
+                    // 作成したオブジェクトを子として登録
+                    obj.transform.parent = transform;
+                    color = true;
+                    Pnum = 0;
+                }
+                else if (Pnum == 3)
+                {
+                    // プレハブからインスタンスを生成
+                    GameObject obj = (GameObject)Instantiate(player3, playerpos, playerrot);
+                    // 作成したオブジェクトを子として登録
+                    obj.transform.parent = transform;
+                    color = true;
+                    Pnum = 0;
+                }
+                else if (Pnum == 4)
+                {
+                    // プレハブからインスタンスを生成
+                    GameObject obj = (GameObject)Instantiate(player4, playerpos, playerrot);
+                    // 作成したオブジェクトを子として登録
+                    obj.transform.parent = transform;
+                    color = true;
+                    Pnum = 0;
+                }
+                else if (Pnum == 5)
+                {
+                    // プレハブからインスタンスを生成
+                    GameObject obj = (GameObject)Instantiate(player5, playerpos, playerrot);
+                    // 作成したオブジェクトを子として登録
+                    obj.transform.parent = transform;
+                    color = true;
+                    Pnum = 0;
+                }
+            }
+        }
+
+    }
 }
