bf0138a33dae8810ae02caa30cff869ecb826a11
MOROHOSHI TOMOYA <pandaaa0909@gmail.com>
2016-11-18 14:00:38 +0900

    文字の生成方向の訂正

--- Unity/Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/TextScript.cs
+++ Unity/Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/TextScript.cs
@@ -1,23 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-public class textScript : MonoBehaviour {
-	
 
-	public Text text;
+public class TextScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
-	void TextField(string message){
-		text.text = message;
-	}
-
-
 }
