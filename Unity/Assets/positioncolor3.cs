87c071af41bdb48d2847c111108075305d0bbd08
butasimeji <ueda.yuki@entcomp.esys.tsukuba.ac.jp>
2016-11-19 02:06:48 +0900

    色分け完成
    位置によって各色のボディの大きさを変えることで色分けを行った。
    これまでの手法では上手くいかなかったので、違う手法でやったら上手くいった。

---
+++ Unity/Assets/positioncolor3.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class positioncolor3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = this.transform.position;
+		if (pos.x > 2) {
+			this.transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
+		} else {
+			this.transform.localScale = new Vector3 (0.001f, 0.001f, 0.001f);
+		}
+	}
+}
